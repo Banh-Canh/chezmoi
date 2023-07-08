18970090341b287c193498f9aa2035eb09dfbae5
Victor Hang <vhvictorhang@gmail.com>
2023-07-08 07:15:57 +0200

    refacto: things

--- run_before_30_install_prompt.sh
+++ run_before_30_install_prompt.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-export ZSH=$HOME/.oh-my-zsh
+set -e
 
+export ZSH=$HOME/.oh-my-zsh
+export ZSH_CUSTOM=$ZSH/custom
+export KEEP_ZSHRC=yes
+export RUNZSH=no
+
+rm -rf $ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
