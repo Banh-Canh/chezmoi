389f250f7ad12501003181b40e9c04827222211f
Victor Hang <vhvictorhang@gmail.com>
2023-07-08 05:22:18 +0200

    feat: add install prompt

---
+++ run_once_install_prompt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ZSH=$HOME/.oh-my-zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
